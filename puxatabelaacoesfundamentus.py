952110fb67c20f18ec9ec9c229d410f138f3d0b7
Gilson de Moura <140326022+61750n@users.noreply.github.com>
2023-10-31 08:21:51 -0300

    Add files via upload

---
+++ puxatabelaacoesfundamentus.py
@@ -0,0 +1,107 @@
+# 1) INSTALAÇÕES INICIAIS
+# pip install pandas
+# pip install selenium
+# pip install webdriver-manager
+# pip install lxml
+
+# 2) IMPORTAÇÕES DAS BIBLIOTECAS
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+# 3) ABRE NAVEGADOR --> ACESSA SITE
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+url = 'https://www.fundamentus.com.br/resultado.php'
+
+driver.get(url)
+
+# 4) LÊ A TABELA DE AÇÕES --> FECHA SITE
+local_tabela = '/html/body/div[1]/div[2]/table'
+
+elemento = driver.find_element("xpath", local_tabela)
+
+html_tabela = elemento.get_attribute('outerHTML')
+
+tabela = pd.read_html(str(html_tabela), thousands = '.', decimal = ',')[0]
+
+
+#EXIBE TABELA IMPORTADA PARA VERIFICAÇÃO SE A IMPORTAÇÃO E LEITURA DERAM CERTO
+# print(tabela)
+
+#EXIBE O TIPO DE DADO QUE TEM NAS COLUNAS DA TABELA. INT É NÚMERO INTEIRO, FLOAT64 É NÚMERO DECIMAL, 
+# tabela.info()
+
+# 5) TRATAMENTO DO TIPO DE DADO DAS COLUNAS. DE OBJECT PARA FLOAT
+#5.1) COLUNA DIV. YIELD
+tabela['Div.Yield'] = tabela['Div.Yield'].str.replace("%", "")
+tabela['Div.Yield'] = tabela['Div.Yield'].str.replace(".", "")
+tabela['Div.Yield'] = tabela['Div.Yield'].str.replace(",", ".")
+tabela['Div.Yield'] = tabela['Div.Yield'].astype(float)
+
+#5.2) COLUNA Mrg Ebit
+tabela['Mrg Ebit'] = tabela['Mrg Ebit'].str.replace("%", "")
+tabela['Mrg Ebit'] = tabela['Mrg Ebit'].str.replace(".", "")
+tabela['Mrg Ebit'] = tabela['Mrg Ebit'].str.replace(",", ".")
+tabela['Mrg Ebit'] = tabela['Mrg Ebit'].astype(float)
+
+#5.3) COLUNA Mrg Ebit
+tabela['Mrg. Líq.'] = tabela['Mrg. Líq.'].str.replace("%", "")
+tabela['Mrg. Líq.'] = tabela['Mrg. Líq.'].str.replace(".", "")
+tabela['Mrg. Líq.'] = tabela['Mrg. Líq.'].str.replace(",", ".")
+tabela['Mrg. Líq.'] = tabela['Mrg. Líq.'].astype(float)
+
+#5.4) COLUNA ROIC
+tabela['ROIC'] = tabela['ROIC'].str.replace("%", "")
+tabela['ROIC'] = tabela['ROIC'].str.replace(".", "")
+tabela['ROIC'] = tabela['ROIC'].str.replace(",", ".")
+tabela['ROIC'] = tabela['ROIC'].astype(float)
+
+#5.5) COLUNA ROE
+tabela['ROE'] = tabela['ROE'].str.replace("%", "")
+tabela['ROE'] = tabela['ROE'].str.replace(".", "")
+tabela['ROE'] = tabela['ROE'].str.replace(",", ".")
+tabela['ROE'] = tabela['ROE'].astype(float)
+
+#5.6) COLUNA Cresc. Rec.5a
+tabela['Cresc. Rec.5a'] = tabela['Cresc. Rec.5a'].str.replace("%", "")
+tabela['Cresc. Rec.5a'] = tabela['Cresc. Rec.5a'].str.replace(".", "")
+tabela['Cresc. Rec.5a'] = tabela['Cresc. Rec.5a'].str.replace(",", ".")
+tabela['Cresc. Rec.5a'] = tabela['Cresc. Rec.5a'].astype(float)
+
+#6) ELIMINANDO EMPRESAS COM LIQUIDEZ DIÁRIA ABAIXO DE r$1.000.000,00. 
+# ISTO SIGNIFICA QUE É RÁPIDO E FÁCIL COMPRAR E VENDER ESSAS AÇÕES
+tabela = tabela[tabela['Liq.2meses'] > 1000000]
+
+#7) ELIMINANDO EMPRESAS COM EV/EBIT BAIXO OU NEGATIVO
+tabela = tabela[tabela['EV/EBIT'] > 0]
+
+#8) ELIMINANDO EMPRESAS COM ROIC BAIXO OU NEGATIVO
+tabela = tabela[tabela['ROIC'] > 0]
+
+#9) ELIMINANDO EMPRESAS COM P/VP MAIOR QUE 1
+tabela = tabela[tabela['P/VP'] < 1]
+
+#10) ELIMINANDO EMPRESAS COM ...... MAIOR QUE .....
+#tabela = tabela[tabela['P/VP'] < 1]
+
+#11) CLASSIFICA TABELA PELO EV/EBIT EM ORDEM CRESCENTE (MENOR PARA O MAIOR)
+tabela['ranking_ev_ebit'] = tabela['EV/EBIT'].rank(ascending = True)
+
+#11) CLASSIFICA TABELA PELO ROIC EM ORDEM DECRESCENTE (MAIOR PARA O MENOR)
+tabela['ranking_roic'] = tabela['ROIC'].rank(ascending = False)
+
+#12) CLASSIFICA TABELA PELO DIVIDEND YIELD EM ORDEM DECRESCENTE (MAIOR PARA O MENOR)
+tabela['ranking_dividend'] = tabela['Div.Yield'].rank(ascending = False)
+
+#13) CONSTRUÇÃO DO RANKING
+tabela['ranking_total'] = tabela['ranking_ev_ebit'] + tabela['ranking_roic'] + tabela['ranking_dividend']
+
+tabela = tabela.sort_values('ranking_total')
+
+#14) EXIBE AS 10 MELHORES AÇÕES:
+print(tabela.head(10))
+
+#15) EXPORTA PARA EXCEL
+tabela.to_excel('C:/Users/gilso/OneDrive/Programacao/FIIs e Acoes/top_10_acoes.xlsx', index=False)
---
+++ readme.md
@@ -0,0 +1,59 @@
+# TOP 10 ACOES
+
+Este arquivo README fornece uma visão geral do projeto.
+
+## O que o projeto faz?
+
+O projeto TOP 10 ACOES faz um ranking diário das 10 melhores AÇÕES usando alguns elementos da análise fundamentalista:
+1- Empresas com Liquidez Bimestral superior a R$1.000.000,00;
+2- Empresas com EV/EBIT não zerados ou negativos;
+3- Empresas com ROIC não zerados ou negativos;
+4- Empresas com P/VP menor que 1;
+
+As empresas da lista estão classificada dessa forma:
+1- Por EV/EBIT;
+2- Por ROIC;
+3- Por Dividend Yiueld;
+
+A lista gerada é exportada como arquivo Excel (xlsx)
+
+
+## Por que o projeto é útil?
+
+O projeto é útil porque lista empresas com um arcabouço interessante para investimento, apesar de não tratar de recomendação de investimento.
+
+## Como os usuários podem começar com o projeto?
+
+Para começar com o projeto, siga estas etapas:
+
+1. Instalação do Python
+2. Instalação das bibliotecas pandas, selenium, webdriver-manager e lxml
+3. Acesso à Internet
+
+## Onde os usuários podem obter ajuda com o projeto?
+
+Os usuários podem obter ajuda com o projeto nos seguintes locais:
+
+* gilson.moura@gmail.com
+
+## Quem mantém e contribui para o projeto?
+
+O projeto é mantido por Gilson de Moura.
+
+## Licença
+
+O projeto é licenciado sob a GNU GPL v3.
+
+## Contribuições
+
+Contribuições são bem-vindas! Para contribuir com o projeto, siga estas etapas:
+
+1. Faça um fork do repositório.
+2. Crie uma branch para sua contribuição.
+3. Faça suas alterações.
+4. Faça um commit de suas alterações.
+5. Envie um pull request para a branch principal.
+
+## Versão
+
+Esta é a versão 1.0
